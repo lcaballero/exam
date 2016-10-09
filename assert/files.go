2fe2fceff0324ca3a330ad812852448b64514b10
Lucas Caballero <caballero.luke@gmail.com>
2016-10-09 17:36:55 -0600

    Fixes that help clarify things.

--- assert/files.go
+++ assert/files.go
@@ -6,13 +6,22 @@ import (
 	"testing"
 )
 
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 func Exists(t *testing.T, root string, files ...string) {
 	for _, file := range files {
 		dir := filepath.Join(root, file)
-		_, err := os.Stat(dir)
-		if os.IsNotExist(err) {
-			t.Fail()
+		if FileExists(dir) {
+			continue
 		}
+		t.Logf("File '%s' does not exist.", dir)
+		t.Fail()
 	}
 }
 
